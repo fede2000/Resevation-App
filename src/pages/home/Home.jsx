0b814a1ce2bb0ee8b126c17309e846e946905589
federico almaraz <federicoalmaraz37@gmail.com>
2023-10-30 13:48:41 -0300

    featured components - featuredProperties component - propertyList component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,6 @@
+
+import Featured from "../../components/featured/Featured"
+import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
 import "./home.css"
 
@@ -5,7 +8,11 @@ import "./home.css"
 const Home = () => {
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar/>
+      <Header/>
+      <div className="homeContainer">
+        <Featured/>
+      </div>
     </div>
   )
 }
