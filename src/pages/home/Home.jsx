659ba071998fb0cf8e5808f15dcd99ea6a2acbf9
federico almaraz <federicoalmaraz37@gmail.com>
2023-10-30 14:32:23 -0300

    mainList conponent

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,10 @@
 
 import Featured from "../../components/featured/Featured"
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
 import Header from "../../components/header/Header"
+import MailList from "../../components/mailList/MailList"
 import Navbar from "../../components/navbar/Navbar"
+import PropertyList from "../../components/propertyList/PropertyList"
 import "./home.css"
 
 
@@ -12,6 +15,11 @@ const Home = () => {
       <Header/>
       <div className="homeContainer">
         <Featured/>
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties/>
+        <MailList/>
       </div>
     </div>
   )
